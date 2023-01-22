7cd03bec3a67a57ed916833a6a875ce31ce6c0ad
UliGaMi <ulises.galvez@hotmail.com>
2023-01-21 18:37:29 -0600

    [ADD] Imágen personaI

--- src/components/molecules/InfoBar.jsx
+++ src/components/molecules/InfoBar.jsx
@@ -1,12 +1,13 @@
 import CircleImg from "../atoms/CircleImg";
 import '../../assets/styles/InfoBar.css';
 import Info from "../atoms/info";
+import ImgPersonal from "../../assets/imgs/Foto.jpg"
 
 function InfoBar({person}) {
     return ( 
         <div className="infoBar">
             <div className="principal">
-                <CircleImg route={person.img}></CircleImg>
+                <CircleImg route={ImgPersonal}></CircleImg>
                 <h2 className="nombre">{person.name}</h2>   
             </div>
             <Info person={person}></Info>
